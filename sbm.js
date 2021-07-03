a0c9faf1116741751bce8f55160c2bcfc82ae2f4
NinaKonecna <skdragontv09@gmail.com>
2021-07-03 20:06:41 +0200

    massive add

--- sbm.js
+++ sbm.js
@@ -1,4 +1,5 @@
 phrase="aHVieQ=="
+phrase2="YXR0ZXZhcmI="
 function sbm2(){
   document.getElementById("checker").onkeyup = function(e){
     if(e.keyCode == 13){
@@ -12,5 +13,18 @@ function sbm(){
     document.getElementById("Menu").style.display="block"
     document.getElementById("bg-image").style.display="block"
   }
-
+}
+function sbm4(){
+  document.getElementById("checker").onkeyup = function(e){
+    if(e.keyCode == 13){
+      sbm3();
+    }
+  }
+}
+function sbm3(){
+  if(document.getElementById("checker").value==atob(phrase2)){
+    document.getElementById("Blocker").style.display="none"
+    document.getElementById("Menu").style.display="block"
+    document.getElementById("bg-image").style.display="block"
+  }
 }
